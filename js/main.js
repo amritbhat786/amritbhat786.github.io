2ee128a31de0a2245ddb3368f6fcf31e75898cad
Amrit_Bhat <amrit.bhat@analyttica.com>
2019-12-20 17:25:11 +0530

    Added skills, Temp: education and profile picture

--- js/main.js
+++ js/main.js
@@ -286,6 +286,56 @@
 		})
 	};
 
+	// var timeLine() = function() {
+	// 	window.sr = ScrollReveal();
+
+	// 	  if ($(window).width() < 768) {
+
+	// 	  	if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+	// 	  		$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+	// 	  	}
+
+	// 	  	sr.reveal('.js--fadeInRight', {
+	// 		    origin: 'right',
+	// 		    distance: '300px',
+	// 		    easing: 'ease-in-out',
+	// 		    duration: 800,
+	// 		  });
+
+	// 	  } else {
+		  	
+	// 	  	sr.reveal('.js--fadeInLeft', {
+	// 		    origin: 'left',
+	// 		    distance: '300px',
+	// 			  easing: 'ease-in-out',
+	// 		    duration: 800,
+	// 		  });
+
+	// 		  sr.reveal('.js--fadeInRight', {
+	// 		    origin: 'right',
+	// 		    distance: '300px',
+	// 		    easing: 'ease-in-out',
+	// 		    duration: 800,
+	// 		  });
+
+	// 	  }
+		  
+	// 	  sr.reveal('.js--fadeInLeft', {
+	// 		    origin: 'left',
+	// 		    distance: '300px',
+	// 			  easing: 'ease-in-out',
+	// 		    duration: 800,
+	// 		  });
+
+	// 		  sr.reveal('.js--fadeInRight', {
+	// 		    origin: 'right',
+	// 		    distance: '300px',
+	// 		    easing: 'ease-in-out',
+	// 		    duration: 800,
+	// 		  });
+
+	// }
+
 	// Document on load.
 	$(function(){
 		fullHeight();
@@ -304,6 +354,7 @@
 		sliderMain();
 		stickyFunction();
 		owlCrouselFeatureSlide();
+		// timeLine();
 	});
 
 
